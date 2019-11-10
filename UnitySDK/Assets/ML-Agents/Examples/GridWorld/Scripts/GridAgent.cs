55859593deddbcf5859089f23e6b3c7479e6b91b
hminsky <henry.minsky@gmail.com>
2019-11-10 14:27:05 -0500

    turn GridWorld into a WAMP pubsub client watcher, subscribed to ai.leela.sms.render.json topic

--- UnitySDK/Assets/ML-Agents/Examples/GridWorld/Scripts/GridAgent.cs
+++ UnitySDK/Assets/ML-Agents/Examples/GridWorld/Scripts/GridAgent.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using UnityEngine;
-using System.Linq;
 using MLAgents;
 using System.Collections.Generic;
 using Blocksworld;
+
+using System.Collections.Generic;
+using System.Text;
 
 
 public class GridAgent : Agent
@@ -27,7 +29,7 @@ public class GridAgent : Agent
     public override void InitializeAgent()
     {
         academy = FindObjectOfType(typeof(GridAcademy)) as GridAcademy;
-        blocksworldSMS = new BlocksWorldSensoriMotorSystem(academy.gridSize);
+        //blocksworldSMS = new BlocksWorldSensoriMotorSystem(academy.gridSize);
 
 
        
@@ -55,7 +57,7 @@ public class GridAgent : Agent
      
     }
    
-    BlocksWorldSensoriMotorSystem blocksworldSMS;
+    //BlocksWorldSensoriMotorSystem blocksworldSMS;
 
     // to be implemented by the developer
     public override void AgentAction(float[] vectorAction, string textAction)
@@ -68,8 +70,8 @@ public class GridAgent : Agent
         actions.Add(textAction);
 
         // returns a string encoding sensors and values e.g., "hp11=0;hp21=1;hp31=0;..."
-        sensorObservationsText = blocksworldSMS.stepPhysicalWorld("0", actions);
-        Debug.Log(sensorObservationsText);
+        //sensorObservationsText = blocksworldSMS.stepPhysicalWorld("0", actions);
+        Debug.Log(textAction);
 
         /*
          * Here we need to poke into blocksworld to get locations of the hand and the blocks
@@ -77,20 +79,79 @@ public class GridAgent : Agent
          */
 
         // -->>> grab block and hand locs and set tranforms <<<--
-        copyBlocksPositionsToUnity();
+        copyBlocksPositionsToUnity(textAction);
 
 
 
     }
 
-    public void copyBlocksPositionsToUnity()
+    [System.Serializable]
+        public struct ObjInfo
     {
-        Vec2 handpos = blocksworldSMS.hand1.getPosition();
-        Vec2 eyepos = blocksworldSMS.eye.getPosition();
-        Vec2 block1pos = blocksworldSMS.block1.getPosition();
-        Vec2 block2pos = blocksworldSMS.block2.getPosition();
-        Vec2 block3pos = blocksworldSMS.block3.getPosition();
-        Vec2 block4pos = blocksworldSMS.block4.getPosition();
+        public string name;
+        public int x;
+        public int y;
+    }
+
+    // convert {"name": "b2", "x": 5, "y": 5} to  a Vec2(5,5); 
+    public Vec2 makePositionVector(ObjInfo obj)
+    {
+        Vec2 v = new Vec2();
+        v.x =  obj.x;
+        v.y = obj.y;
+        return v;
+    }
+
+    [System.Serializable]
+    public class ObjLocs
+    { 
+
+        public ObjInfo[] objs;
+       
+        public static ObjLocs CreateFromJSON(string jsonString)
+        {
+            return JsonUtility.FromJson<ObjLocs>(jsonString);
+        }
+
+        public ObjInfo getByName(string name)
+        {
+            foreach (ObjInfo obj in objs)
+            {
+                if (obj.name == name)
+                {
+                    return obj;
+                }
+            }
+            throw new Exception("could not find any debuginfo objloc object named " + name);
+        }
+
+        // Given JSON input:
+        // {"name":"Dr Charles","lives":3,"health":0.8}
+        // this example will return a PlayerInfo object with
+        // name == "Dr Charles", lives == 3, and health == 0.8f.
+    }
+    /*
+      Incoming textAction contains object locations in json format list of objs:
+
+     [{"name": "h", "x": 4, "y": 3, "color": "rgb(0,0,255)", "shape": null, "texture": "02", "rotation": 0, "grasping": true, "graspedObject": null}, {"name": "j", "x": -100, "y": -100, "color": "rgb(255,165,0)", "shape": null, "texture": "03", "rotation": 0, "grasping": false, "graspedObject": null}, {"name": "v", "x": 0, "y": 4, "color": "rgb(255,255,255)", "shape": "circle", "texture": "00"}, {"name": "b1", "x": 1, "y": 5, "color": "rgb(255,20,147)", "shape": "circle", "texture": "00"},
+     {"name": "b2", "x": 5, "y": 5, "color": "rgb(255,255,0)", "shape": "square", "texture": "01"}]
+      
+     */
+
+
+
+    public void copyBlocksPositionsToUnity(string textAction)
+    {
+        ObjLocs objlocs = ObjLocs.CreateFromJSON(textAction);
+        Debug.Log("textAction=" + textAction);
+   
+
+        Vec2 handpos = makePositionVector(objlocs.getByName("h"));
+        Vec2 eyepos = makePositionVector(objlocs.getByName("v"));
+        Vec2 block1pos = makePositionVector(objlocs.getByName("b1"));
+        Vec2 block2pos = makePositionVector(objlocs.getByName("b2"));
+        Vec2 block3pos = new Vec2(100,100);
+        Vec2 block4pos = new Vec2(100,100);
 
         float dx = 0.0f;
         float dy = 0.0f;
